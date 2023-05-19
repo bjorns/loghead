d409a433646f50d0fe7acb01cff46d5f1305ec41
Björn Skoglund <bjorn.skoglund@compass.com>
2023-05-18 21:19:51 -0400

    Fix pylint warnings in event class

--- loghead/event.py
+++ loghead/event.py
@@ -9,6 +9,11 @@ _local_tz = datetime.utcnow().astimezone().tzinfo
 
 
 class Event:
+    """
+    Represents a log event, created automatically by the Log implementation. In
+    the LogPipeline it's created and then passed through all components during
+    processing.
+    """
     def __init__(self, level: Level, msg: str):
         self.timestamp = datetime.now(_local_tz)
         self.level = level
