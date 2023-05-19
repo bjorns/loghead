0136024ea34ce7a89300a25b3c5d4cc34ee3982e
Björn Skoglund <bjorn.skoglund@compass.com>
2023-05-18 21:28:05 -0400

    Fix config pylint warnings

--- loghead/config.py
+++ loghead/config.py
@@ -11,6 +11,7 @@ import yaml
 
 class PipelineConfig:
     """
+    Config for a single pipeline description in the larger Config object.
     """
 
     def __init__(self, name: str, level: str, form: str):
@@ -36,6 +37,10 @@ class Config:
         return f"Config<{self.filepath}>"
 
     def update(self, other):
+        """
+        Update the config object by merging the parameter objects properties
+        into the subject.
+        """
         self.filepath = other.filepath
         self.filename = other.filename
         self.pipelines = other.pipelines
@@ -45,34 +50,49 @@ def load_config(path: str) -> Config:
     """
     Load config file to Config object.
     """
-    with open(path, "r") as f:
+    with open(path, "r", encoding='utf-8') as f:
         conf = yaml.safe_load(f)
         pipelines = parse_pipelines(conf)
         config = Config(path, pipelines)
         return config
 
 
-def parse_pipeline_config(name, pipeline_data: dict) -> PipelineConfig:
+def parse_pipeline_config(name: str, pipeline_data: dict) -> PipelineConfig:
+    """
+    Generate a PipelineConfig object from the raw data read from the Yaml/Json
+    file.
+
+    :param name: The name of the pipeline
+    :param pipeline_data: the data read from Yaml.
+    """
     level = parse_level(pipeline_data)
     formatter = parse_format(pipeline_data)
-    p = PipelineConfig(name, level=level, form=formatter)
-    return p
+    return PipelineConfig(name, level=level, form=formatter)
 
 
 def parse_pipelines(conf: dict) -> list[PipelineConfig]:
-    ret = list()
+    """
+    Parse the pipelines defined in the config data.
+    """
+    ret = []
     for name, pipeline_data in conf.items():
-        p = parse_pipeline_config(name, pipeline_data)
-        ret.append(p)
+        pipeline_config = parse_pipeline_config(name, pipeline_data)
+        ret.append(pipeline_config)
     return ret
 
 
 def parse_level(pipeline_data: dict) -> str:
+    """
+    Read the log level configured in the pipeline, defaults to 'info'.
+    """
     val = pipeline_data.get("level", "info")
     # todo: validate
     return val
 
 
 def parse_format(pipeline_data: dict) -> str:
+    """
+    Read the format configured in the pipeline config. Default to 'simple'.
+    """
     val = pipeline_data.get("format", "simple")
     return val
