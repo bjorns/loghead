6e691346e3b0b39875a594d9cf1020f4e386b9d1
Björn Skoglund <bjorn.skoglund@compass.com>
2023-05-21 15:14:15 -0400

    Fix pylint error in watcher module

--- loghead/watch.py
+++ loghead/watch.py
@@ -1,4 +1,7 @@
-from watchdog.events import FileSystemEventHandler, LoggingEventHandler, RegexMatchingEventHandler
+"""
+Monitor the changes of a config and triger a reload when changed.
+"""
+from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 from watchdog.observers.polling import PollingObserver
 
@@ -14,19 +17,15 @@ class ConfigEventHandler(FileSystemEventHandler):
     file event handler interface in order to get
     file save events from the source config file
     """
-
-    def __init__(self, filepath: str):
-        super(ConfigEventHandler, self).__init__()
-
     def on_modified(self, event):
         """
         File was modified
         """
         try:
-            super(ConfigEventHandler, self).on_modified(event)
+            super().on_modified(event)
             debuglog("file modified event: %s", event.src_path)
-            c = load_config(event.src_path)
-            load_environment(c)
+            config = load_config(event.src_path)
+            load_environment(config)
         except UserError as e:
             debuglog("error: %s", e)
 
@@ -35,21 +34,27 @@ class ConfigEventHandler(FileSystemEventHandler):
         For whatever reason a file save often shows up as a file created event.
         """
         try:
-            super(ConfigEventHandler, self).on_created(event)
+            super().on_created(event)
             debuglog("file created event: %s", event.src_path)
-            c = load_config(event.src_path)
-            load_environment(c)
+            config = load_config(event.src_path)
+            load_environment(config)
         except UserError as e:
             debuglog("error: %s", e)
 
 
 class ConfigFileWatchdog:
+    """
+    Wraps and configures the observer object.
+    """
     def __init__(self, config: Config, observer: Observer = None, event_handler=None):
-        event_handler = event_handler or ConfigEventHandler(config)
+        event_handler = event_handler or ConfigEventHandler()
         self.observer = observer or PollingObserver()
         self.observer.schedule(event_handler, config.filepath)
         self.filepath = config.filepath
 
     def sit_booboo(self):
+        """
+        Start the watchdog thread
+        """
         debuglog("starting filewatcher %s", self.filepath)
         self.observer.start()
